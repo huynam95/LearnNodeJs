715edcb0886998de715f04993cf34f9536b9a312
huynam95 <tranhuynam15@gmail.com>
2023-04-13 07:48:13 +0700

    Update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,12 +8,16 @@ router.post('/signup', authController.signUp);
 router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
+
 router.patch(
   '/updatePassword',
   authController.protect,
   authController.updatePassword
 );
+router.patch('/updateMe', authController.protect, userController.updateMe);
 router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
 router
   .route('/')
